b4d1630e6b11ab853fb2a2fc5dd5cc90256f688f
EvanDLang <evan.d.lang@nasa.gov>
2023-02-28 15:31:10 +0000

    read the docs configuration testing

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
-from setuptools import setup, find_packages
-
+from setuptools import setup
+  
 setup(
     name='shift_python_utilities',
     version='0.0.1',
-    packages=find_packages()
+    description='Modules to help make common data operations easier',
+    author='Evan Lang',
+    author_email='evan.d.lang@nasa.gov',
+    packages=['shift_utilities'],
+    install_requires=[],
 )
