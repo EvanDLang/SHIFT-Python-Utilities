ffa166c34651a935138191d10f8cf302fcbc3414
EvanDLang <evan.d.lang@nasa.gov>
2023-02-28 15:55:35 +0000

    read the docs configuration testing

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
-  
-setup(
-    name='shift_python_utilities',
-    version='0.0.1',
-    description='Modules to help make common data operations easier',
-    author='Evan Lang',
-    author_email='evan.d.lang@nasa.gov',
-    packages=['shift_utilities'],
-    install_requires=[],
-)
+
+setup(name='shift_python_utilities',
+      version=__version__,
+      url='http://github.com/isofit/isofit/',
+      author='Evan Lang',
+      author_email='evan.d.lang@nasa.gov',
+      description='Modules to help make common data operations easier',
+      packages=find_packages(),
+      install_requires=[],
+      python_requires='>=3.7')
