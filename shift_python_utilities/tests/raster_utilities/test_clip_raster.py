fd888005c38ee025749798381d3053c338b6988f
Evan Lang <evan.d.lang@nasa.gov>
2023-03-21 14:17:16 +0000

    Finalized the inital version of the shift catalog, updated the file stucture

---
+++ shift_python_utilities/tests/raster_utilities/test_clip_raster.py
@@ -0,0 +1,51 @@
+import pytest
+import rasterio as rio
+import geopandas as gpd
+import os
+from shift_python_utilities.raster_utilities import clip_raster
+
+root_dir = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
+root_dir = os.path.join(root_dir, "test_data")
+
+@pytest.fixture
+def geodf():
+    shapefile = os.path.join(root_dir,"testing_shape_file", "testing_shape_file.shp")
+    return gpd.read_file(shapefile)
+
+def clip_raster_driver(geodf):
+    in_path = os.path.join(root_dir, "test_raster_rotated")
+    out_path = os.path.join(root_dir, "test_raster_rotated_clipped")
+    
+    with rio.open(in_path) as src:
+        src_profile = src.profile
+    
+    try:
+        clip_raster(in_path, geodf, out_path)
+    except:
+        for file in [out_path, out_path + ".hdr", out_path + ".aux.xml"]:
+            if os.path.isfile(file):
+                os.remove(file)
+        return False
+    
+    with rio.open(out_path) as src:
+        dst_profile = src.profile
+    
+    os.remove(out_path)
+    os.remove(out_path + ".hdr")
+    os.remove(out_path + ".aux.xml")
+    
+    return src_profile, dst_profile
+ 
+    
+def test_clip_raster(geodf):
+    # fails because of crs differences
+    assert not clip_raster_driver(geodf)
+    
+    src, dst = clip_raster_driver(geodf.to_crs(geodf.estimate_utm_crs(datum_name='WGS 84')))
+    a1, b1, _, d1, e1, _ , _ , _ ,_ = src['transform']
+    a2, b2, _, d2, e2, _ , _ , _ ,_ = dst['transform']
+    assert a1 == a2
+    assert b1 == b2
+    assert d1 == d2
+    assert e1 == e2
+
---
+++ shift_python_utilities/tests/raster_utilities/test_reproject_raster.py
@@ -0,0 +1,53 @@
+import pytest
+import rasterio as rio
+import os
+from shift_python_utilities.raster_utilities import reproject_raster
+
+root_dir = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
+root_dir = os.path.join(root_dir, "test_data")
+
+
+def reproject_raster_driver(**kwargs):
+    in_path = os.path.join(root_dir, "test_raster_rotated")
+    out_path =os.path.join(root_dir, "test_raster_rotated_output")
+    
+    with rio.open(in_path) as src:
+        src_profile =src.profile
+    
+    try:
+        reproject_raster(in_path,out_path, **kwargs)
+    except:
+        for file in [out_path, out_path + ".hdr", out_path + ".aux.xml"]:
+            if os.path.isfile(file):
+                os.remove(file)
+        raise Exception("Reprojection failed!")
+                
+    with rio.open(out_path) as src:
+        dst_profile = src.profile
+    
+    os.remove(out_path)
+    os.remove(out_path + ".hdr")
+    os.remove(out_path + ".aux.xml")
+    
+    return src_profile, dst_profile
+ 
+
+@pytest.mark.parametrize(
+    "crs, resampling_method, resolution, expected_transform, expected_crs",
+    [
+        (None, 'nearest', None, (2.6, -0., -0., -2.6), "EPSG:32616"),
+        ("EPSG:32616", 'nearest', None, (2.6, -0. , -0., -2.6), "EPSG:32616"),
+        ("EPSG:4326", 'nearest', (3.7, 3.7), (3.7, -0., -0., -3.7), ("EPSG:4326", "OGC:CRS84")),
+    ],
+)
+
+def test_reproject_raster(crs, resampling_method, resolution ,expected_transform, expected_crs):
+    src, dst = reproject_raster_driver(crs=crs, resampling_method=resampling_method, resolution=resolution)
+    a,b,_,d,e,_,_,_,_ = dst['transform']
+    assert (a,b, d, e) == expected_transform
+    if len(expected_crs) == 2:
+        expected_crs_1, expected_crs_2 = expected_crs 
+        assert dst['crs'].to_string() == expected_crs_1 or dst['crs'].to_string() == expected_crs_2
+    else:
+        assert dst['crs'].to_string() == expected_crs
+
