5aaaa42aa466f5b87d2b57d30dcd11b84993fefe
Evan Lang <evan.d.lang@nasa.gov>
2023-03-29 14:34:02 +0000

    Updated the intake_shift driver to include igm data when reading in raw data. Wrote tests for intake_shift. Added bad band filtering. Fixed bug with the progress bar for raster reprojection. Orthorectification with intake_shift is still a WIP.

--- shift_python_utilities/intake_shift/shift_xarray.py
+++ shift_python_utilities/intake_shift/shift_xarray.py
@@ -6,11 +6,14 @@ from intake_xarray.base import Schema
 import glob
 import os
 import xarray as xr
+import warnings
+import rasterio
+warnings.filterwarnings("ignore", category=rasterio.errors.NotGeoreferencedWarning)
 
 class ShiftXarray(RasterIOSource):
     name = 'SHIFT_xarray'
 
-    def __init__(self, urlpath, ortho=False, chunks=None, concat_dim='concat_dim',
+    def __init__(self, urlpath, ortho=False, filter_bad_bands=False, chunks=None, concat_dim='concat_dim',
                  xarray_kwargs=None, metadata=None, path_as_pattern=True,
                  storage_options=None, **kwargs):
       
@@ -19,12 +22,32 @@ class ShiftXarray(RasterIOSource):
                  storage_options=storage_options, **kwargs)
         
         self.ortho = ortho
+        self.filter_bad_bands = filter_bad_bands
+    
+    def _get_supporting_file(self, path, name):
+        # parse filepath
+        base, file = os.path.split(path)
+        base, directory = os.path.split(base)
+        key = {'L2a': 'reflectance', 'rdn': 'radiance', 'obs': 'obs', 'igm': 'igm'}
+        data_var = key[directory]     
         
-        if ortho:
-            import warnings
-            import rasterio
-            warnings.filterwarnings("ignore", category=rasterio.errors.NotGeoreferencedWarning)
+        # edit filepath to point to glt file
+        if directory != "L2a":
+            directory = name
+        else:
+            directory = os.path.join("L1", name)
+        
+        file_base = file.split('_')[0]
+        file = file_base + '_' + name
+        s_path = os.path.join(base, directory, file)
 
+        return s_path, data_var
+      
+    def _filter_bad_bands(self):
+        from .bad_bands import bad_bands
+        self._ds = self._ds.isel(wavelength=bad_bands)
+        
+    
     def _open_files(self, files):
         das = [xr.open_rasterio(f, chunks=self.chunks, **self._kwargs)
                for f in files]
@@ -52,12 +75,38 @@ class ShiftXarray(RasterIOSource):
             files = self.urlpath
             #files = fsspec.open(self.urlpath, **self.storage_options).open()
         if isinstance(files, list):
-            self._ds = self._open_files(files)
+            # self._ds = self._open_files(files)
+            print('Multi-file loading is currently not supported')
         else:
             self._ds = xr.open_rasterio(files, chunks=self.chunks,
                                         **self._kwargs).swap_dims({"band":"wavelength"}).drop("band").transpose('y', 'x', 'wavelength')
-            if self.ortho:
+            if (not "igm" in files and not "glt" in files) and not self.ortho:
+                igm_path, _ = self._get_supporting_file(files, 'igm')
+                
+                try:
+                    igm = xr.open_rasterio(igm_path, **self._kwargs).swap_dims({"band":"wavelength"}).drop("band").transpose('y', 'x', 'wavelength')
+
+                    lon = igm.isel(wavelength=0).values
+                    lat = igm.isel(wavelength=1).values
+                    elev = igm.isel(wavelength=2).values
+
+                    self._ds = self._ds.assign_coords({'lat':(['y','x'], lat)})
+                    self._ds = self._ds.assign_coords({'lon':(['y','x'], lon)})
+                    self._ds = self._ds.assign_coords({'elevation':(['y','x'], elev)})
+                except:
+                    print("Error configuring igm data")
+                    
+            if self.filter_bad_bands and (not "igm" in files and not "glt" in files and not "obs" in files):
+                self._filter_bad_bands()
+            
+            if self.ortho and not "glt" in files:
                 self._orthorectify()
+            elif self.ortho and "glt" in files:
+                print("glt file cannot be orthorectified")
+                # to a dataset?
+            else:
+                pass
+                # to a dataset?
     
     
     def _get_schema(self):
@@ -89,29 +138,13 @@ class ShiftXarray(RasterIOSource):
         return self._schema
     
     def _orthorectify(self):
-        # parse filepath
-        base, file = os.path.split(self.urlpath)
-        base, directory = os.path.split(base)
-        key = {'L2a': 'reflectance', 'rdn': 'radiance'}
-        
-        # currently only reflectance and radiance files have been tested
-        assert directory in key, "Currenly only reflectance and radiance files are supported"
-        data_var = key[directory]     
-        
-        # edit filepath to point to glt file
-        if directory != "L2a":
-            directory = "glt"
-        else:
-            directory = os.path.join("L1", 'glt')
-        
-        file_base = file.split('_')[0]
-        file = file_base + "_glt"
-        glt_path = os.path.join(base, directory, file)
+        # get the path to the GLT file
+        glt_path, data_var = self._get_supporting_file(self.urlpath, 'glt')
         
         # verify a glt file exists
         assert os.path.exists(glt_path), "No glt file exists, File cannot be orthorectified"
         
-        #parse glt file
+        # parse glt file
         loc = xr.open_rasterio(glt_path).transpose('y', 'x', 'band')
         glt_array = loc.values.astype(int)
         
@@ -131,21 +164,37 @@ class ShiftXarray(RasterIOSource):
             if valid_glt[x,:].sum() != 0:
                 y = valid_glt[x,:]
                 out_ds[x, y, :] = ds_array[glt_array[x, y, 1], glt_array[x, y, 0], :]
- 
+         
+        GT = loc.transform
+        
+        dim_x = loc.x.shape[0]
+        dim_y = loc.y.shape[0]
+        lon = np.zeros(dim_x)
+        lat = np.zeros(dim_y)
+        
+        for x in np.arange(dim_x):
+            x_geo = GT[2] + x * GT[0]
+            lon[x] = x_geo
+        for y in np.arange(dim_y):
+            y_geo = GT[5] + y * GT[4]
+            lat[y] = y_geo
+        
+        
         # Set up the metadata for the output
         wvl = self._ds.wavelength.values
-        fwhm = self._ds.fwhm.values 
+   
         metadata = {
             'description': self._ds.attrs['description'],
             'lines': loc.attrs['lines'],
             'samples': loc.attrs['samples'],
             'is_tiled ': self._ds.attrs['is_tiled'],           
             'bands': self._ds.attrs['bands'],
-            'interleave': self._ds.attrs['interleave'],
+            'interleave': 'bil',
             'data_type': self._ds.attrs['data_type'],
             'file_type': self._ds.attrs['file_type'],
             'transform': loc.attrs['transform'],
             'res': loc.attrs['res'],
+            'map info': loc.attrs['map_info'],
             'coordinate_system_string': loc.attrs['coordinate_system_string'],
             'nodatavals': self._ds.attrs['nodatavals'],
             'descriptions': wvl,
@@ -154,9 +203,13 @@ class ShiftXarray(RasterIOSource):
         
         
         # create coords and data vars for output
-        coords = {'y':(['y'], loc.y.data), 'x':(['x'], loc.x.data), 'wavelength':(['wavelength'], wvl)}
-        data_vars = {data_var:(['y','x','wavelength'], out_ds)}
+        coords = {'lat':(['lat'], lat), 'lon':(['lon'], lon), 'wavelength':(['wavelength'], wvl)}
+        data_vars = {data_var:(['lat','lon','wavelength'], out_ds)}
         
         # create the output xarray dataset
         self._ds = xr.Dataset(data_vars=data_vars, coords=coords, attrs=metadata)
-        self._ds = self._ds.assign_coords(fwhm=("wavelength", fwhm))
+        try:
+            fwhm = self._ds.fwhm.values 
+            self._ds = self._ds.assign_coords(fwhm=("wavelength", fwhm))
+        except:
+            pass # no fwhm coord
--- shift_python_utilities/tests/intake_shift/test_shift_catalog.py
+++ shift_python_utilities/tests/intake_shift/test_shift_catalog.py
@@ -1,17 +1,96 @@
 import pytest
+import numpy as np
 from shift_python_utilities.intake_shift import shift_catalog
 
 @pytest.fixture
 def cat():
     return shift_catalog()
 
-
-def test_clip_raster(cat):
-    # Verify each type of dataset can be read in
+def test_aviris_data(cat):
     cat.aviris_v1_gridded().read_chunked()
-    cat.L2a(date="20220228", time="183924").read_chunked()
-    cat.L1.rdn(date="20220228", time="183924").read_chunked()
-    cat.L1.glt(date="20220228", time="183924").read_chunked()
-    cat.L1.obs(date="20220228", time="183924").read_chunked()
-    cat.L1.igm(date="20220228", time="183924").read_chunked()
+
     
+@pytest.fixture
+def band_dict():
+    return {
+        'L2a': 425,
+        'rdn': 425,
+        'igm': 3,
+        'glt': 2,
+        'obs': 11
+    }
+@pytest.fixture
+def band_dict_filtered():
+    return {
+        'L2a': 337,
+        'rdn': 337,
+        'igm': 3,
+        'glt': 2,
+        'obs': 11,
+    }
+
+
+@pytest.mark.parametrize(
+    "dataset, date, time, ortho, filter_bad_bands",
+    [
+        (("L1", "glt"), "20220228", "183924", False, False),
+        (("L1", "glt"), "20220228", "183924", False, True),
+        (("L1", "glt"), "20220228", "183924", True, True),
+        (("L1", "igm"), "20220228", "183924", False, False),
+        (("L1", "igm"), "20220228", "183924", False, True),
+        (("L1", "igm"), "20220228", "183924", True, True),
+        (("L1", "obs"), "20220228", "183924", False, False),
+        (("L1", "obs"), "20220228", "183924", False, True),
+        (("L1", "obs"), "20220228", "183924", True, True),
+        (("L1", "rdn"), "20220228", "183924", False, False),
+        (("L1", "rdn"), "20220228", "183924", False, True),
+        # (("L1", "rdn"), "20220228", "183924", True, True),
+        # ("L2a", "20220228", "183924", True, False),
+        ("L2a", "20220228", "183924", False, False),
+        ("L2a", "20220228", "183924", False, True)
+    ],
+)
+
+def test_intake_shift_driver(cat, band_dict, band_dict_filtered, dataset, date, time, ortho, filter_bad_bands):
+    # Verify each type of dataset can be read in
+    if isinstance(dataset, tuple):
+        parent, child = dataset
+        key = child
+        dataset = getattr(getattr(cat, parent), child)()
+    else:
+        key = dataset
+        dataset = getattr(cat, dataset)()
+    
+    ds = dataset(date=date, time=time, ortho=ortho, filter_bad_bands=filter_bad_bands).read_chunked()
+    
+    if filter_bad_bands:
+        bands = band_dict_filtered 
+        assert bands[key] == len(ds.wavelength)
+    else:
+        bands = band_dict 
+        assert bands[key] == len(ds.wavelength)
+        
+    if ortho:
+        glt = cat.L1.glt()(date=date, time=time, ortho=ortho).read_chunked()
+        if key == 'glt':
+            assert glt.values.shape == ds.values.shape
+        else:
+            y, x, z = glt.shape
+            glt_array = glt.values.astype(int)
+            valid_glt = np.all(glt_array != -9999, axis=-1)
+            glt_array[valid_glt] -= 1 
+
+
+            for x in range(valid_glt.shape[0]):
+                if valid_glt[x,:].sum() == 0:
+                    continue
+                else:
+                    break
+            y = valid_glt[x, :]
+            test_cord = glt_array[x, y, :][0]
+
+            original_data = dataset(date=date, time=time, filter_bad_bands=filter_bad_bands).read_chunked()
+
+            var = {'L2a': 'reflectance', 'rdn': 'radiance', 'obs': 'obs', 'igm': 'igm'}
+            assert np.count_nonzero(np.nan_to_num(getattr(ds, var[key]).values,nan=-9999) != -9999.) ==  np.count_nonzero(valid_glt) * bands[key]
+            assert (np.round(original_data.values[test_cord[1], test_cord[0]]) == np.round(getattr(ds, var[key]).values[x, y, :][0])).all()
